52fdb2eaf819038f4c2fe78b9c5c52fc78584913
resita-alterra <resita@alterra.id>
2019-08-13 11:53:50 +0700

    nambah link

--- listing_news_api/my-app/src/components/List.js
+++ listing_news_api/my-app/src/components/List.js
@@ -29,17 +29,19 @@ export class List extends React.Component {
       return (
         <li className="list-group-item">
           <div className="one_list">
-            <p
-              style={{
-                backgroundColor: "red",
-                width: 30,
-                borderRadius: 4,
-                textAlign: "center"
-              }}
-              className="list"
-            >
-              #{key + 1}
-            </p>
+            <a href={elm.url} target="_blank">
+              <p
+                style={{
+                  backgroundColor: "red",
+                  width: 30,
+                  borderRadius: 4,
+                  textAlign: "center"
+                }}
+                className="list"
+              >
+                #{key + 1}
+              </p>
+            </a>
             <a href={elm.url} target="_blank">
               <p className="list" style={{ fontWeight: "700" }}>
                 {elm.title}
